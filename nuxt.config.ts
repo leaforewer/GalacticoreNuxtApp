84ba359d8b232c005a1fdca07b4fd9da988f4f3a
Ahmet Cemal Erdogan <56025913+leaforewer@users.noreply.github.com>
2023-04-15 18:09:44 +0200

    Create nuxtjs.yml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  target: 'static',
-  router:{
-    base: '/GalacticoreNuxtApp/',
-  },
-  app:{
-    baseURL: '/GalacticoreNuxtApp/'
-  },
   modules: ["@nuxtjs/tailwindcss"],
   css: ["@/assets/css/tailwind.css"],
 });
