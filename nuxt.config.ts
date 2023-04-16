b6059f275af8def3dff841163ad6e14fd091fa96
Ahmet Erdogan <ahmeterdogan@Ahmets-MBP.home>
2023-04-16 18:10:40 +0200

    Deploy Nuxt3 to Github Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
+  router:{
+    base: '/GalacticoreNuxtApp/'
+  },
   app:{
     baseURL: 'GalacticoreNuxtApp'
   },
